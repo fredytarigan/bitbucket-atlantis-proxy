9af806803aedbe10fe0cb6f83e0c3841805d5a9e
Fredy Samuel B Tarigan <fredy.tarigan@ovo.id>
2020-03-24 20:41:30 +0700

    Add route

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -12,4 +13,9 @@ func main() {
 	server := mux.NewRouter()
 
 	server.HandleFunc("/", index).Methods(http.MethodGet)
+	server.HandleFunc("/ping", ping).Methods(http.MethodGet)
+	server.HandleFunc("/healthz", healthz).Methods(http.MethodGet)
+	server.HandleFunc("/hook", hook).Methods(http.MethodPost)
+
+	log.Fatal(http.ListenAndServe(":8080", server))
 }
--- cmd/proxy/server.go
+++ cmd/proxy/server.go
@@ -1,13 +1,36 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+type StandardResponse struct {
+	Message    string
+	StatusCode string
+}
+
+const bitbucketEventTypeHeader = "X-Event-Key"
+const bitbucketCloudRequestIDHeader = "X-Request-UUID"
 
 /*
 Home handler
 URI Path : "/"
 */
 func index(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data := StandardResponse{
+		Message:    "Welcome !!!",
+		StatusCode: "200",
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(jsonData)
 }
 
 /*
@@ -15,7 +38,17 @@ Healthz handler
 URI Path : "/healthz"
 */
 func healthz(w http.ResponseWriter, r *http.Request) {
-
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data := StandardResponse{
+		Message:    "Application is healthy",
+		StatusCode: "200",
+	}
+	jsonData, err := json.Marshal(data)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w.Write(jsonData)
 }
 
 /*
@@ -25,7 +58,7 @@ URI Path : "/ping"
 func ping(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"pong"}`))
+	w.Write([]byte(`"pong"`))
 }
 
 /*
@@ -33,5 +66,24 @@ Hook handler
 URI Path : "/hook"
 */
 func hook(w http.ResponseWriter, r *http.Request) {
+	eventType := r.Header.Get(bitbucketEventTypeHeader)
 
+	defer r.Body.Close()
+	body, err := ioutil.ReadAll(r.Body)
+
+	if err != nil {
+		data := StandardResponse{
+			Message:    "Unable to read data body: ",
+			StatusCode: "504",
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+
+		jsonData, _ := json.Marshal(data)
+		w.Write(jsonData)
+		log.Fatal(err)
+	}
+
+	log.Println(eventType)
+	log.Println(body)
 }
