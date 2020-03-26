e5a8893cf02100696951d5bcc7bbb4b71fc098d9
Fredy Samuel B Tarigan <fredy.tarigan@ovo.id>
2020-03-26 21:17:33 +0700

    Try parse the commit hash

--- cmd/proxy/helper.go
+++ cmd/proxy/helper.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"os/user"
+
 	"gopkg.in/src-d/go-git.v4"
 	"gopkg.in/src-d/go-git.v4/plumbing"
+	"gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
 )
 
 func gitClone(c string) error {
+	currentUser, err := user.Current()
+
+	if err != nil {
+		return err
+	}
+
+	sshAuth, err := ssh.NewPublicKeysFromFile("git", currentUser.HomeDir+"/.ssh/id_rsa", "")
+
+	if err != nil {
+		return err
+	}
+
 	// Clone the given repository to the given directory
 	r, err := git.PlainClone("/opt/terraform", false, &git.CloneOptions{
-		URL: "git@bitbucket.org:ovoeng/terraform.git",
+		URL:  "git@bitbucket.org:ovoeng/terraform.git",
+		Auth: sshAuth,
 	})
 
 	if err != nil {
