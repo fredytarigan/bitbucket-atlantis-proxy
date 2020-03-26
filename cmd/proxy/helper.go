7a2c92fc0498f71e227b486ecf148d76bc69cb24
Fredy Samuel B Tarigan <fredy.tarigan@ovo.id>
2020-03-26 22:48:30 +0700

    Try parse the commit hash

--- cmd/proxy/helper.go
+++ cmd/proxy/helper.go
@@ -1,56 +1,52 @@
 package main
 
 import (
-	"log"
-	"os/user"
-
-	"golang.org/x/crypto/ssh"
-	"gopkg.in/src-d/go-git.v4"
-	"gopkg.in/src-d/go-git.v4/plumbing"
-	gossh "gopkg.in/src-d/go-git.v4/plumbing/transport/ssh"
+	"io"
+	"os"
+	"os/exec"
 )
 
+func IsDirEmpty(name string) (bool, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return false, err
+	}
+	defer f.Close()
+
+	// read in ONLY one file
+	_, err = f.Readdir(1)
+
+	// and if the file is EOF... well, the dir is empty.
+	if err == io.EOF {
+		return true, nil
+	}
+	return false, err
+}
+
 func gitClone(c string) error {
-	log.Printf("Commit Sent : %s", c)
-	currentUser, err := user.Current()
+	repoURL := "git@bitbucket.org:ovoeng/terraform.git"
+	cloneDir := "/opt/terraform"
+
+	// check if directory exists and not empty
+	ok, err := IsDirEmpty("/opt/terraform")
 
 	if err != nil {
 		return err
 	}
 
-	sshAuth, err := gossh.NewPublicKeysFromFile("git", currentUser.HomeDir+"/.ssh/id_rsa", "")
-	sshAuth.HostKeyCallback = ssh.InsecureIgnoreHostKey()
-
-	if err != nil {
-		return err
+	if ok {
+		// Clone the given repository to the given directory
+		_ = exec.Command("git", "clone", "--branch", "master", "--single-branch", repoURL, cloneDir)
 	}
 
-	// Clone the given repository to the given directory
-	r, err := git.PlainClone("/opt/terraform", false, &git.CloneOptions{
-		URL:               "git@bitbucket.org:ovoeng/terraform.git",
-		RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
-		Auth:              sshAuth,
-	})
+	// run a pull
+	cmd := exec.Command("git", "pull")
+	cmd.Dir = cloneDir
 
-	if err != nil {
-		return err
-	}
-
-	w, err := r.Worktree()
-	// handle error
-	if err != nil {
-		return err
-	}
-
-	// ... checking out to commit
-	err = w.Checkout(&git.CheckoutOptions{
-		Hash: plumbing.NewHash(c),
-	})
-
-	// handle error
-	if err != nil {
-		return err
-	}
+	// checkout to a given commit
+	cmd = exec.Command("git", "checkout", c)
+	cmd.Dir = cloneDir
 
 	return nil
+
 }
