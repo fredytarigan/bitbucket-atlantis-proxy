7187293ec263a3c3e0412d4fdea33609dd87574b
Fredy Samuel B Tarigan <fredy.tarigan@ovo.id>
2020-03-29 14:27:05 +0700

    Fixing checkout errors

--- cmd/proxy/server.go
+++ cmd/proxy/server.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
-	"io/ioutil"
 	"log"
 	"net/http"
-	"time"
 )
 
 type StandardResponse struct {
@@ -86,16 +83,18 @@ URI Path : "/hook"
 func hook(w http.ResponseWriter, r *http.Request) {
 	var c CommitHashID
 	var atlantisURL string
-	var requestBody []byte
 
 	eventType := r.Header.Get(bitbucketEventTypeHeader)
 
+	defer r.Body.Close()
+
 	// store the body
 	// this will be the data we sent to atlantis
-	body, err := ioutil.ReadAll(r.Body)
+	// body, err := ioutil.ReadAll(r.Body)
 
-	requestBody = body
 	requestHeader := r.Header
+
+	log.Printf("request header %s", requestHeader)
 
 	err = json.NewDecoder(r.Body).Decode(&c)
 
@@ -136,37 +135,35 @@ func hook(w http.ResponseWriter, r *http.Request) {
 
 	if atlantisURL != "" {
 		log.Printf("Proxying bitbucket hook to atlantis server at %s", atlantisURL)
-		timeout := time.Duration(5 * time.Second)
-		client := http.Client{
-			Timeout: timeout,
-		}
+		// timeout := time.Duration(5 * time.Second)
+		// client := http.Client{
+		// 	Timeout: timeout,
+		// }
 
-		request, err := http.NewRequest("POST", atlantisURL+"/events/", bytes.NewBuffer(requestBody))
+		// request, err := http.NewRequest("POST", atlantisURL+"/events/", bytes.NewBuffer(requestBody))
 
-		for key, values := range requestHeader {
-			for _, value := range values {
-				request.Header.Set(key, value)
-			}
-		}
+		// for key, values := range requestHeader {
+		// 	for _, value := range values {
+		// 		request.Header.Set(key, value)
+		// 	}
+		// }
 
-		if err != nil {
-			log.Printf("Error when sending request to atlantis %s", err)
-		}
+		// if err != nil {
+		// 	log.Printf("Error when sending request to atlantis %s", err)
+		// }
 
-		resp, err := client.Do(request)
+		// resp, err := client.Do(request)
 
-		if err != nil {
-			log.Printf("Got error from response %s", err)
-		}
+		// if err != nil {
+		// 	log.Printf("Got error from response %s", err)
+		// }
 
-		defer resp.Body.Close()
+		// defer resp.Body.Close()
 
-		log.Printf("Response status from atlantis %v", resp.StatusCode)
+		// log.Printf("Response status from atlantis %v", resp.StatusCode)
 
 	} else {
 		log.Printf("Cannot find atlantis URL for environment %s", environment)
 	}
-
-	defer r.Body.Close()
 
 }
